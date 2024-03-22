fdb24114a981dcdda59d003039c4d1b158bd9e5c
NaNinMind <rezhikov.d99@gmail.com>
2024-03-22 19:07:31 +0300

    new line

--- JAQpApi/src/main/java/com/example/JAQpApi/JaQpApiApplication.java
+++ JAQpApi/src/main/java/com/example/JAQpApi/JaQpApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class JaQpApiApplication
